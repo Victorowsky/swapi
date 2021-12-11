8e6778bda4a91f4dfb372e9e470c84d74fe114d7
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-12-11 18:21:20 +0100

    Header link to '/'

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,22 @@
 import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface HeaderProps {}
+
+const classes = {
+	link: {
+		textDecoration: "none",
+		color: "inherit",
+	},
+};
 
 const Header: React.FC<HeaderProps> = () => {
 	return (
 		<Box>
 			<Typography variant="h3" align="center">
-				Star Wars Wiki
+				<Link style={classes.link} to="/">
+					Star Wars Wiki
+				</Link>
 			</Typography>
 		</Box>
 	);
