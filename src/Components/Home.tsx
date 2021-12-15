6ec136c2e321da63e29278ba48076d324153ae81
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-12-15 13:16:12 +0100

    searchValue for all components,

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,3 +1,5 @@
+import { TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import ChooseCategory from "./ChooseCategory";
@@ -13,36 +15,52 @@ interface HomeProps {}
 const Home: React.FC<HomeProps> = () => {
 	const { category } = useSelector((state: RootState) => state.api);
 
+	const [searchValue, setSearchValue] = useState("");
+
+	const handleChangeValue = (
+		e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+	) => {
+		setSearchValue(e.target.value);
+	};
+
+	useEffect(() => {}, []);
+
 	let currentComponent: JSX.Element;
 
 	switch (category) {
 		case "characters":
-			currentComponent = <PeopleView />;
+			currentComponent = <PeopleView searchValue={searchValue} />;
 			break;
 		case "planets":
-			currentComponent = <PlanetsView />;
+			currentComponent = <PlanetsView searchValue={searchValue} />;
 			break;
 		case "species":
-			currentComponent = <SpeciesView />;
+			currentComponent = <SpeciesView searchValue={searchValue} />;
 			break;
 		case "films":
-			currentComponent = <FilmsView />;
+			currentComponent = <FilmsView searchValue={searchValue} />;
 			break;
 		case "starships":
-			currentComponent = <StarshipsView />;
+			currentComponent = <StarshipsView searchValue={searchValue} />;
 			break;
 		case "vehicles":
-			currentComponent = <VehiclesView />;
+			currentComponent = <VehiclesView searchValue={searchValue} />;
 			break;
 
 		default:
-			currentComponent = <PeopleView />;
+			currentComponent = <PeopleView searchValue={searchValue} />;
 			break;
 	}
 
 	return (
 		<>
 			<ChooseCategory />
+			<TextField
+				variant="outlined"
+				placeholder="Search by name"
+				value={searchValue}
+				onChange={handleChangeValue}
+			/>
 			{currentComponent}
 		</>
 	);
